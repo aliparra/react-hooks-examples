c0724865ac6ce562009701f64d9d7b63f49b3bcb
Alicia Parra <alicia@redparra.com>
2023-02-18 18:54:21 +0100

    (simple-form-component) :sparkless: Add simple form component

---
+++ src/components/molecules/simple-form/SimpleForm.tsx
@@ -0,0 +1,56 @@
+import { ReactElement, useState } from 'react';
+
+/**
+ * The function returns a SimpleForm component that displays a simple form with name, email and password.
+ * @returns A SimpleForm without props
+ */
+
+// Declare state for each form value
+export const SimpleForm = (): ReactElement => {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  // Set a new state when the form value changes
+  const onInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target;
+    setFormState({
+      //Only change the formState value that changes.
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const { name, email, password } = formState;
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="form-control mt-2"
+        placeholder="name"
+        name="email"
+        value={name}
+        onChange={onInputChange}
+      />
+      <input
+        type="email"
+        className="form-control mt-2"
+        placeholder="example@example.com"
+        name="email"
+        value={email}
+        onChange={onInputChange}
+      />
+      <input
+        type="password"
+        className="form-control mt-2"
+        placeholder="password"
+        name="email"
+        value={password}
+        onChange={onInputChange}
+      />
+    </div>
+  );
+};
